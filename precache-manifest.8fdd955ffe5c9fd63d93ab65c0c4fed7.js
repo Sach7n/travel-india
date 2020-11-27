6c86fd8176319c793f6688ee7079322235e1ded5
Sachin Macwan <sachinmacwan@gmail.com>
2020-11-27 16:36:53 +1100

    Updates

---
+++ precache-manifest.8fdd955ffe5c9fd63d93ab65c0c4fed7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20221b61b02271f3568174b5ebeb2ba2",
+    "url": "/travel-ind/index.html"
+  },
+  {
+    "revision": "e75f5746842d0e2b3370",
+    "url": "/travel-ind/static/css/main.41b30b45.chunk.css"
+  },
+  {
+    "revision": "c04575c4166c3c21f7df",
+    "url": "/travel-ind/static/js/2.3ed85111.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel-ind/static/js/2.3ed85111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75f5746842d0e2b3370",
+    "url": "/travel-ind/static/js/main.4890c2a2.chunk.js"
+  },
+  {
+    "revision": "b0f001def8e1f5da9e01",
+    "url": "/travel-ind/static/js/runtime-main.66b50f27.js"
+  },
+  {
+    "revision": "03c71b5260b028adbe496109606d5a21",
+    "url": "/travel-ind/static/media/india.03c71b52.png"
+  }
+]);
